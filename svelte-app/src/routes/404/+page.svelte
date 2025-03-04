c8e0c9b7e4e4ca2a01755a99a35c117612e67eab
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2025-03-04 12:23:59 +0000

    small fixes

--- svelte-app/src/routes/404/+page.svelte
+++ svelte-app/src/routes/404/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log("404 Page Loaded");
+    })
+</script>
+
 <h1>Welcome to our Website</h1>
 <p>This is a temporary 404 page.</p>
--- svelte-app/src/routes/cart/+page.svelte
+++ svelte-app/src/routes/cart/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log("Cart Page Loaded");
+    })
+</script>
+
 <h1>Welcome to our Website</h1>
 <p>This is a temporary cart page.</p>

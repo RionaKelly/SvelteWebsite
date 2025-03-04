1cd6f63e9a7dcfa323a077e4321d4f86af333d0a
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2025-03-04 13:48:32 +0000

    temp logo and header added

---
+++ svelte-app/src/lib/components/Header.svelte
@@ -0,0 +1,40 @@
+<script>
+    // import Navigation.svelte from lib folder
+    import { Navigation } from "$lib"
+</script>
+
+
+<header>
+    <!-- Logo -->
+    <div class="logo">
+        <img src="/logo.png" alt="Website Logo">
+        <h1>My Svelte App</h1>
+    </div>
+
+    <!-- Navigation -->
+    <Navigation />
+</header>
+
+
+<style>
+    /* Header Styling */
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #343a40;
+        color: white;
+        padding: 1rem;
+    }
+
+    /* Logo Container */
+    .logo {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo img {
+        width: 50px;
+        margin-right: 0.625rem;
+    }
+</style>

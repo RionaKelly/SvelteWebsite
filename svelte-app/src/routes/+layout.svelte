62bb426f186bd2f49e826e60895bc44050f27014
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2025-03-04 12:54:09 +0000

    layout finished

---
+++ svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    // Import components
+    import { Header, Footer } from "$lib";
+</script>
+
+<!-- Header -->
+<Header />
+
+<!-- Content -->
+<mains>
+    <slot />
+</mains>
+
+<!-- Footer -->
+<Footer />
+
+<style>
+    main {
+        padding: 2rem;
+        text-align: center;
+    }
+</style>

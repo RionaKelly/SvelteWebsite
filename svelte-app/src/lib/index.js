2bd66289e468ebd6d5d1cac622b2523d36703ef0
(Student  C00301164) Carla Epifanio Vivero <C00301164@setu.ie>
2025-03-14 12:10:06 +0000

    Modal component

--- svelte-app/src/lib/index.js
+++ svelte-app/src/lib/index.js
@@ -1,8 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
+// @ts-ignore
 export { default as Header } from "./components/Header.svelte";
+// @ts-ignore
 export { default as Navigation } from "./components/Navigation.svelte";
+// @ts-ignore
 export { default as NavigationFooter } from "./components/NavigationFooter.svelte";
+// @ts-ignore
 export { default as Footer } from "./components/Footer.svelte";
+// @ts-ignore
 export { default as Card } from "./components/Navigation.svelte";
+// @ts-ignore
+export { default as Modal } from "./components/Modal.svelte";
 
-import {Header, Navigation, NavigationFooter, Footer, Card} from "$lib";
+import {Header, Navigation, NavigationFooter, Footer, Card, Modal} from "$lib";

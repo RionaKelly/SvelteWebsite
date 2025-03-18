ca9f8e1c1dbd31a746ebb7d59a074a886e6b5009
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2025-03-18 14:49:09 +0000

    icon transition finished

--- svelte-app/src/lib/components/Header.svelte
+++ svelte-app/src/lib/components/Header.svelte
@@ -21,10 +21,10 @@
 <style>
     /* Header Styling */
     header {
+        background-image: linear-gradient(rgb(203, 123, 225), rgb(213, 143, 235), rgb(223, 153, 245), rgb(213, 143, 235), rgb(203, 123, 225));
         display: flex; /* Enables Flexbox */
         align-items: center; /* Vertically centers the items */
         justify-content: space-between; /* Pushes logo to left, nav to right */
-        background-image: linear-gradient(rgb(203, 123, 225), rgb(213, 143, 235), rgb(223, 153, 245), rgb(213, 143, 235), rgb(203, 123, 225));
         color: white;
         padding: 1rem;
     }
@@ -38,10 +38,15 @@
     .logo img {
         width: 50px;
         margin-right: 10px;
+        opacity: 1;
+        transition: 0.2s ease-in-out;
     }
 
     .logo img:hover {
-        width: 70px;
+        width: 48px;
+        margin-right: 12px;
+        opacity: 0.9;
+        margin-left: 1px;
 }
 
     /* Mobile: Center logo and stack items */

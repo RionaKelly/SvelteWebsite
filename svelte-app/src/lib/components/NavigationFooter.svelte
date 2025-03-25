ea049535e2459290d77eaf389ec749abb32614ee
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2025-03-25 12:18:13 +0000

    header and footer transition made

--- svelte-app/src/lib/components/NavigationFooter.svelte
+++ svelte-app/src/lib/components/NavigationFooter.svelte
@@ -22,11 +22,22 @@
 
     nav ul li {
         margin: 0 15px;
+        transition: 0.15s ease-in-out;
     }
 
     nav ul li a {
         text-decoration: none;
         color: white;
         font-weight: bold;
+        opacity: 1;
+        transition: 0.15s ease-in-out;
+    }
+
+    .nav ul li a:hover {
+        opacity: 0.9;
+        text-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+    }
+    .nav ul li:hover {
+        transform: translateY(1px);
     }
 </style>
